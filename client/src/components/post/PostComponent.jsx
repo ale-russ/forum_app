58102363348cc407c3f3db7b81a13687d9c87907
ale-russ <agarrussom@gmail.com>
2024-08-20 00:18:09 +0300

    working on deleting post

--- client/src/components/post/PostComponent.jsx
+++ client/src/components/post/PostComponent.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
-import { CiHeart } from "react-icons/ci";
-import { FaHeart } from "react-icons/fa";
-import { formatDistanceToNow } from "date-fns";
-import io from "socket.io-client";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from 'react';
+import { CiHeart } from 'react-icons/ci';
+import { FaHeart } from 'react-icons/fa';
+import { formatDistanceToNow } from 'date-fns';
+import io from 'socket.io-client';
+import { useNavigate } from 'react-router-dom';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
-import { useForum } from "../../utils/PostContext";
-import CommentsModal from "./CommentsModal";
-import { host } from "../../utils/ApiRoutes";
-import { updatePost, updateViewCount } from "../../controllers/ForumController";
-import ProfileImage from "../common/ProfileImage";
-import Loader from "../common/Loader";
+import { useForum } from '../../utils/PostContext';
+import CommentsModal from './CommentsModal';
+import { host } from '../../utils/ApiRoutes';
+import { updatePost, updateViewCount } from '../../controllers/ForumController';
+import ProfileImage from '../common/ProfileImage';
+import Loader from '../common/Loader';
 
 const socket = io(host);
 
@@ -21,10 +22,11 @@ const PostComponent = ({ post }) => {
   const [likeCount, setLikeCount] = useState(post?.likes?.length);
   const [isLiked, setIsLiked] = useState(post.likes?.includes(user._id));
   const [viewCount, setViewCount] = useState(post.views.length || 0);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const [localCommentCount, setLocalCommentCount] = useState(
-    post.comments?.length || 0
-  );
+  const [localCommentCount, setLocalCommentCount] = useState(post.comments?.length || 0);
+
+  const handleDeleteUser = () => {};
 
   const handleLike = async () => {
     try {
@@ -32,7 +34,7 @@ const PostComponent = ({ post }) => {
       setIsLiked(updatedPost?.likes.includes(user._id));
       setLikeCount(updatedPost?.likes?.length);
     } catch (err) {
-      console.error("Error updating like:", err);
+      console.error('Error updating like:', err);
     }
   };
 
@@ -54,10 +56,10 @@ const PostComponent = ({ post }) => {
       }
     };
 
-    socket.on("new comment", handleNewComment);
+    socket.on('new comment', handleNewComment);
 
     return () => {
-      socket.off("new comment", handleNewComment);
+      socket.off('new comment', handleNewComment);
     };
   }, [post._id]);
 
@@ -69,18 +71,20 @@ const PostComponent = ({ post }) => {
             <div className="line-clamp-2 font-bold w-[90%]">
               {post?.title} : {post?.content}
             </div>
-            <div
-              className="hidden md:block lg:block xl:block"
-              onClick={handleLike}
-            >
+            <div className="hidden md:block lg:block xl:block" onClick={handleLike}>
               {isLiked ? (
-                <FaHeart
-                  className={`cursor-pointer ${isLiked ? "text-red-500" : ""}`}
-                />
+                <FaHeart className={`cursor-pointer ${isLiked ? 'text-red-500' : ''}`} />
               ) : (
                 <CiHeart className={`cursor-pointer `} />
               )}
             </div>
+            <BsThreeDotsVertical className="cursor-pointer" onClick={() => setShowMenu(!showMenu)} />
+            {showMenu ? (
+              <div className=" relative  z-50 flex flex-col items-center h-24 w-40 top-8 p-2 space-y-2 light-search">
+                {/* <div className="rounded-lg light-navbar w-full p-1 light-navbar">Update post</div> */}
+                <div className="rounded-lg light-navbar w-full p-1 cursor-pointer">Delete post</div>
+              </div>
+            ) : null}
             <div className="block md:hidden lg:hidden xl:hidden">
               <ProfileImage author={post?.author} />
             </div>
@@ -89,10 +93,7 @@ const PostComponent = ({ post }) => {
             {post?.tags &&
               post.tags.map((tag, index) => {
                 return (
-                  <div
-                    key={index}
-                    className="rounded-lg shadow-lg text-[10px] p-2 light-search"
-                  >
+                  <div key={index} className="rounded-lg shadow-lg text-[10px] p-2 light-search">
                     {tag}
                   </div>
                 );
@@ -104,15 +105,11 @@ const PostComponent = ({ post }) => {
             <ProfileImage author={post?.author} />
             <div className="flex flex-col items-start">
               <div className="font-bold text-sm">{post?.author.userName}</div>
-              <div className="text-[10px] text-[#48494e]">
-                {formatDistanceToNow(new Date(post?.createdAt))} ago
-              </div>
+              <div className="text-[10px] text-[#48494e]">{formatDistanceToNow(new Date(post?.createdAt))} ago</div>
             </div>
           </div>
           <div className="flex items-center justify-between text-[10px] text-[#48494e] w-full md:w-[50%] lg:w-[50%] xl:w-[50%]">
-            <div className="flex flex-wrap items-center px-2">
-              {viewCount} View
-            </div>
+            <div className="flex flex-wrap items-center px-2">{viewCount} View</div>
             <div className="flex flex-wrap cursor-pointer" onClick={handleLike}>
               {likeCount} Likes
             </div>
