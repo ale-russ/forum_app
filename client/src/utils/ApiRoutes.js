08da7c26b3949e0f6387e4de628bf35011a9a4c8
ale-russ <agarrussom@gmail.com>
2024-08-29 16:34:58 +0300

    changing api to render

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:4000";
-// export const host = "https://forumapp-4xwf.onrender.com";
+// export const host = "http://localhost:4000";
+export const host = "https://forumapp-4xwf.onrender.com";
 
 export const registerRoute = `${host}/auth/register`;
 export const loginRoute = `${host}/auth/login`;
